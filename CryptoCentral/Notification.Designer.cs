791aa41a14732cf44a3e6ed8a0f09f99fc8bb4a4
Justin Lee <justinivip@gmail.com>
2018-03-14 13:33:38 +1100

    Login //002

---
+++ CryptoCentral/Notification.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace CryptoCentral
+{
+    partial class Notification
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.RoundedCorners = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.SuspendLayout();
+            // 
+            // RoundedCorners
+            // 
+            this.RoundedCorners.ElipseRadius = 5;
+            this.RoundedCorners.TargetControl = this;
+            // 
+            // Notification
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Orange;
+            this.ClientSize = new System.Drawing.Size(424, 94);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Notification";
+            this.Text = "Notification";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuElipse RoundedCorners;
+    }
+}
